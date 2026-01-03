187f8d80d3b3dc373ce086615f37262d2fb50372
Sneha Bhunia <sneharanibhunia26@gmail.com>
2026-01-03 22:14:24 +0530

    View tasks feature implemented

--- todo.py
+++ todo.py
@@ -1,5 +1,19 @@
 tasks = []
 
+
+def add_task():
+    task = input("Enter task: ")
+    tasks.append(task)
+    print("Task added successfully")
+
+def view_tasks():
+    if not tasks:
+        print("No tasks found")
+    else:
+        for i, task in enumerate(tasks, start=1):
+            print(f"{i}. {task}")
+
+
 def menu():
     print("1. Add Task")
     print("2. View Tasks")
@@ -11,11 +25,9 @@ while True:
 
     if choice == "1":
         add_task()
-    
+
+    if choice == "2":
+        view_tasks()
+
     if choice == "3":
         break
-
-def add_task():
-    task = input("Enter task: ")
-    tasks.append(task)
-    print("Task added successfully")
